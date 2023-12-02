8058cee8372d94140977eea8e4db8dca2752bd93
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-12-02 15:16:53 +0700

    Disabled, Hidden, Checked only using property binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,16 +6,23 @@ import { Component } from "@angular/core";
   styleUrls: ["./product-list.component.css"],
 })
 export class ProductListComponent {
+  name = "John Doe";
   product = {
     name: "iPhone 13",
     price: 999,
     color: "Red",
     discount: 8.5,
-    inStock: 5,
+    inStock: 0,
     pImage: "/assets/images/iPhone.jpg",
   };
 
   getDiscountedPrice() {
     return this.product.price * (1 - this.product.discount / 100);
+  }
+
+  getProductStock() {
+    return this.product.inStock > 0
+      ? `Only ${this.product.inStock} items left`
+      : "Not in Stock";
   }
 }
