b6128adf3ff076848fb30d250137a6209e8df35f
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-12-08 09:58:25 +0700

    update value for product detail

--- src/app/container/product-detail/product-detail.component.ts
+++ src/app/container/product-detail/product-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { Product } from "src/app/Models/Product";
 
 @Component({
-  selector: 'product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  selector: "product-detail",
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.css"],
 })
 export class ProductDetailComponent {
-
+  @Input()
+  product: Product;
 }
